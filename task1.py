508915937da4e5c8adbcf5634258ecf409ae780d
KirillMaklusov <levcenkokirill317@gmail.com>
2024-10-24 14:03:27 +0300

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,16 @@
+from math import pi
+
+result = round(pi, 2)
+print(result)
+
+
+
+print("Основание натурального логарифма = 2,71828182845904")
+
+
+number = int(input("Введите число: "))
+print("Ваше число - ", number)
+
+
+number = int(input("Введите число: "))
+print(number, " - вот какое число вы ввели")
